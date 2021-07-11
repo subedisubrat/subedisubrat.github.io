4cdaa31684cccad7bc1475818eb55f898f684dcf
Subrat Subedi <subrat_subedi@hotmail.com>
2021-07-11 15:17:32 -0400

    Minor improvements

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
+const isProd = process.env.NODE_ENV === 'production'
+module.exports = {
+  assetPrefix: isProd ? 'https://subedisubrat.github.io' : '',
+}
 module.exports = withNextra()
--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,8 @@ class MyDocument extends Document {
             title: "Subrat Subedi",
             description: "Hello there! My name is Subrat, and I'm currently a senior at University of South Dakota pursuing a major in Computer Science and minor in Mathematics. I enjoy learning and working with data and infrastructure, making it more robust by deep-diving into the system.",
             image:
-                "https://subedisubrat.github.io/images/preview.png"
+                "https://subedisubrat.github.io/images/preview.png",
+            url: "https://subedisubrat.github.io"
         }
 
         return (
@@ -22,6 +23,8 @@ class MyDocument extends Document {
                     <meta property="og:site_name" content={meta.title} />
                     <meta property="og:description" content={meta.description} />
                     <meta property="og:title" content={meta.title} />
+                    <meta property="og:url" content={meta.url} />
+                    <meta property="og:type" content="website" />
                     <meta property="og:image" content={meta.image} />
                     <meta name="twitter:card" content="summary_large_image" />
                     <meta name="twitter:site" content="@subedisubrat" />
